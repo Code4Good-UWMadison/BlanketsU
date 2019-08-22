61573a12b8fb7787f22f2019bd75c39379c0ce47
Chentao Wang <cwang556@wisc.edu>
2019-08-21 21:19:30 -0500

    update donor and donee handlers

--- services/receive.js
+++ services/receive.js
@@ -164,46 +164,23 @@ module.exports = class Receive {
     console.log("inside Donor: ");
     console.log(payload);
     let response;
-    switch (payload) {
-      case "TRACK_ORDER":
-        response = Response.genQuickReply(i18n.__("order.prompt"), [
-          {
-            title: i18n.__("order.account"),
-            payload: "LINK_ORDER"
-          },
-          {
-            title: i18n.__("order.search"),
-            payload: "SEARCH_ORDER"
-          },
-          {
-            title: i18n.__("menu.help"),
-            payload: "CARE_ORDER"
-          }
-        ]);
-        break;
-
-      case "SEARCH_ORDER":
-        response = Response.genText(i18n.__("order.number"));
-        break;
-
-      case "ORDER_NUMBER":
-        response = Response.genImageTemplate(
-          `${config.appUrl}/order.png`,
-          i18n.__("order.status")
-        );
-        break;
-
-      case "LINK_ORDER":
-        response = [
-          Response.genText(i18n.__("order.dialog")),
-          Response.genText(i18n.__("order.searching")),
-          Response.genImageTemplate(
-            `${config.appUrl}/order.png`,
-            i18n.__("order.status")
-          )
-        ];
-        break;
-    }
+    response = [
+      Response.genText(i18n.__("donor.prompt")),
+      Response.genQuickReply(i18n.__("donor.question"), [
+        {
+          title: i18n.__("order.account"),
+          payload: "LINK_ORDER"
+        },
+        {
+          title: i18n.__("order.search"),
+          payload: "SEARCH_ORDER"
+        },
+        {
+          title: i18n.__("menu.help"),
+          payload: "CARE_ORDER"
+        }
+      ])
+    ];
     return response;
   }
 
@@ -211,46 +188,23 @@ module.exports = class Receive {
     console.log("inside Donee: ");
     console.log(payload);
     let response;
-    switch (payload) {
-      case "TRACK_ORDER":
-        response = Response.genQuickReply(i18n.__("order.prompt"), [
-          {
-            title: i18n.__("order.account"),
-            payload: "LINK_ORDER"
-          },
-          {
-            title: i18n.__("order.search"),
-            payload: "SEARCH_ORDER"
-          },
-          {
-            title: i18n.__("menu.help"),
-            payload: "CARE_ORDER"
-          }
-        ]);
-        break;
-
-      case "SEARCH_ORDER":
-        response = Response.genText(i18n.__("order.number"));
-        break;
-
-      case "ORDER_NUMBER":
-        response = Response.genImageTemplate(
-          `${config.appUrl}/order.png`,
-          i18n.__("order.status")
-        );
-        break;
-
-      case "LINK_ORDER":
-        response = [
-          Response.genText(i18n.__("order.dialog")),
-          Response.genText(i18n.__("order.searching")),
-          Response.genImageTemplate(
-            `${config.appUrl}/order.png`,
-            i18n.__("order.status")
-          )
-        ];
-        break;
-    }
+    response = [
+      Response.genText(i18n.__("donee.prompt")),
+      Response.genQuickReply(i18n.__("donee.question"), [
+        {
+          title: i18n.__("order.account"),
+          payload: "LINK_ORDER"
+        },
+        {
+          title: i18n.__("order.search"),
+          payload: "SEARCH_ORDER"
+        },
+        {
+          title: i18n.__("menu.help"),
+          payload: "CARE_ORDER"
+        }
+      ])
+    ];
     return response;
   }
 
